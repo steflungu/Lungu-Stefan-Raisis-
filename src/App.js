ea1610e7e4d120fb5b99e696761d3837bd971f6c
Lungu Stefan <lungustefan24@yahoo.com>
2023-03-31 21:04:17 +0300

    fix header.js problem

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 import AddTask from "./components/AddTask";
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -28,6 +29,8 @@ const App = () => {
   //ADD TASK
   const addTask = (task) => {
     const id = Math.floor(Math.random() + 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
   };
 
   //DELETE TASK
@@ -46,8 +49,8 @@ const App = () => {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask onAdd={addTask} />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
